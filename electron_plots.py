91daa89afc8c0a49a6817df90c1231c204c7d33f
sophiemiddleton <user@MacBook-Pro.local>
2020-04-04 16:52:13 -0500

    added in many features in macros

--- electron_plots.py
+++ electron_plots.py
@@ -5,16 +5,29 @@ data=readLHEF('unweighted_events.lhe')
 electrons=data.getParticlesByIDs([11,-11])
 c=TCanvas()
 c.Divide(2,2)
-hist_e_theta=TH1F("theta_elec", "Electron Theta",100,-1,1)
-hist_4mom=TH2F("4mom ratio", "4mom in v 4mom out", 100,0,100,100,0,100)
-
+hist_e_theta=TH1F("theta_elec", "Outgoing Electron Theta",100,-1,1)
+hist_4in=TH1F("q^{2} in", "Incoming Electron q^{2} ", 100,0,100)
+hist_4out=TH1F("q^{2} out", "Outgoing Electron q^{2} ", 100,0,100)
+hist_4momRatio=TH1F("q^{2} out", "Ratio q^{2} ", 100,0,100)
 for e in electrons:
-    val = math.atan(e.py/e.pz)
-    hist_e_theta.Fill(val)
-    squared_out = 1;
-    squared_in = 1;
-    hist_4mom.Fill(squared_out, squared_in)
+
+    if e.status is -1 :
+        squared_4mom = (e.px*e.px + e.py*e.py+e.pz*e.pz+e.energy*e.energy)
+        hist_4in.Fill(squared_4mom)
+    if e.status is 1:
+        val = math.atan(e.py/e.pz)
+        hist_e_theta.Fill(val)
+        squared_4mom = (e.px*e.px + e.py*e.py+e.pz*e.pz+e.energy*e.energy)
+        hist_4out.Fill(squared_4mom)
+
+
 c.cd(1)
 hist_e_theta.GetXaxis().SetTitle("#theta")
 hist_e_theta.Draw()
+c.cd(2)
+hist_4in.GetXaxis().SetTitle("q^{2}_{e_{in}}")
+hist_4in.Draw()
+c.cd(3)
+hist_4out.GetXaxis().SetTitle("q^{2}_{e_{out}}")
+hist_4out.Draw()
 c.SaveAs("electron_plots.png")
--- readin.py
+++ readin.py
@@ -1,5 +1,5 @@
 from lhereader import readLHEF
-from ROOT import TCanvas, TH1F
+from ROOT import TCanvas, TH1F, TH2F
 import math
 data=readLHEF('unweighted_events.lhe')
 electrons=data.getParticlesByIDs([11,-11])
@@ -7,12 +7,18 @@ photons=data.getParticlesByIDs([22,-22])
 c=TCanvas()
 hist_e=TH1F("costheta_elec", "Cos Theta",100,-1,1)
 hist_photon=TH1F("costheta_photon", "Cos Theta",100,-1,1)
+hist_4mom=TH2F("4mom ratio", "4mom in v 4mom out", 100,0,100,100,0,100)
+
 for e in electrons:
     val = math.atan(e.py/e.pz)
     hist_e.Fill(val)
+    squared_out = 1;
+    squared_in = 1;
+    hist_4mom.Fill(squared_out, squared_in)
 hist_e.Draw()
 for g in photons:
     val = math.atan(g.py/g.pz)
     hist_photon.Fill(val)
-#hist_photon.Draw("same")
+
+hist_photon.Draw("same")
 c.SaveAs("costheta_elec.png")
